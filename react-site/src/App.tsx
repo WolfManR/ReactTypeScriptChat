bf104f2dfc9604373c1dfec82b043a1259574b58
Tarxos <ftarxos@gmail.com>
2022-11-03 01:13:14 +0300

    remove weather forecasts sample from project

--- react-site/src/App.tsx
+++ react-site/src/App.tsx
@@ -1,32 +1,8 @@
-import { useState, useEffect } from 'react'
 import Index from "./chat";
 
-interface Weather {
-  date: string
-  temperatureC: number
-  summary: string
-  temperatureF: number
-}
-
 function App() {
-  const [weather, setWeather] = useState<Array<Weather>>()
-
-  useEffect(() => {
-    fetch('http://localhost:5271/weatherforecast')
-      .then((response) => response.json())
-      .then((json) => setWeather(json as Array<Weather>))
-  }, [])
-
-  if (!weather) return <div className="App">Loading...</div>
-
   return (
     <div className="App">
-        {weather.map((w: Weather, i: number) => (
-                <p key={i}>
-                    {new Date(w.date).toLocaleTimeString()} {w.temperatureC} {w.summary}{' '}
-                    {w.temperatureF}
-                </p>
-                ))}
         <Index/>
     </div>
   )
